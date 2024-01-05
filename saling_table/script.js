7b3608ba10e3700e227d8c35cee8dc90f57a8a39
JUNTHEWORLD <62176354+junfuture1103@users.noreply.github.com>
2024-01-05 11:30:25 +0900

    first commit

---
+++ saling_table/script.js
@@ -0,0 +1,17 @@
+let total = 0;
+
+function updateTotal() {
+
+    if (document.getElementById('option1').checked) {
+        total += 10;
+    }
+    else if  (document.getElementById('option2').checked) {
+        total += 20;
+    }
+    else if  (document.getElementById('option3').checked) {
+        total += 30;
+    }
+    // 다른 옵션들에 대한 가격 계산 추가
+
+    document.getElementById('totalPrice').innerText = '$' + total;
+}
